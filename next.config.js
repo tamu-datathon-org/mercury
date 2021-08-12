103f873cf01464026ab7121f62249f14df2c444e
Dan Abreo <dabreo43@gmail.com>
2021-08-11 21:09:59 -0500

    Revert "cors allowed origin fix"
    This reverts commit cbb76071101faa871491b57edad1a6086e24a1cf.

--- next.config.js
+++ next.config.js
@@ -4,19 +4,6 @@ const defaultBasePath = '/mailing';
 const defaultFallbackUrl = 'https://tamudatathon.com/:path*';
 
 module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://tamudatathon.com" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  },
   sassOptions: {
     includePaths: [path.join(__dirname)]
   },
