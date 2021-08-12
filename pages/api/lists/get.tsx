cd8de3816520413eec9b0b6581e2159283b8b0a5
Dan Abreo <dabreo43@gmail.com>
2021-08-11 21:48:40 -0500

    report invalid api key

--- pages/api/lists/get.tsx
+++ pages/api/lists/get.tsx
@@ -3,8 +3,13 @@ import Mailgun from 'mailgun-js';
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   const headers = res.getHeaders();
-  if (!headers || !headers['x-mercury-api-key'] || headers['x-mercury-api-key'] != process.env.MERCURY_API_KEY) {
+  if (!headers || !headers['x-mercury-api-key']) {
     throw new Error('MERCURY_API_KEY missing or invalid .env.development.local');
+  }
+  if (headers['x-mercury-api-key'] != process.env.MERCURY_API_KEY) {
+    return new Promise(() => {
+      res.status(500).send('MERCURY_API_KEY is not valid');
+    });
   }
   if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
     throw new Error('MAILGUN_API_KEY or MAILGUN_DOMAIN are not defined in .env.development.local');
